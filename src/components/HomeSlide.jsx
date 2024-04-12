694009f77092e1fa61b10be5a82f21e1ef4dfb3a
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2024-04-12 06:33:48 -0700

    started working on responsiveness

--- src/components/HomeSlide.jsx
+++ src/components/HomeSlide.jsx
@@ -22,6 +22,7 @@ const HomeSlide = () => {
     <Swiper
       style={{
         "--swiper-navigation-color" :"#FFFFFF",
+        "--swiper-navigation-size": "30px",
         "--swiper-pagination-color": "#FFFFFF",
         "--swiper-pagination-bullet-inactive-color": "#999999",
         "--swiper-pagination-bullet-inactive-opacity": "1",
@@ -36,32 +37,32 @@ const HomeSlide = () => {
       pagination={{
         clickable: true,
       }}
-      className="rounded-md bg-[#222]"
+      className="rounded-md bg-[#222] h-[70vh]"
     >
       <SwiperSlide>
-        <div className="relative w-full">
-          <img src={slideBg} alt="" className="w-full object-cover"/>
-          <div className="absolute top-[20%] left-[10%] md:left-[10%] right-0 flex flex-col justify-center gap-12  items-center md:items-start">
-            <p className="text-xl md:text-5xl text-white md:leading-[60px]">Bringing the market <br/><span className="text-[#FF931E]">closer</span> to you!</p>
+        <div className="relative w-full h-full">
+          <img src={slideBg} alt="" className="w-full h-full object-cover"/>
+          <div className="absolute top-[30%] left-[15%] right-0 flex flex-col justify-center gap-6 md:gap-12 items-start">
+            <p className="text-2xl md:text-5xl text-white leading-[30px] md:leading-[60px]">Bringing the market <br/><span className="text-[#FF931E]">closer</span> to you!</p>
             <button className="text-white bg-[#FF931E] text-sm md:text-[18px] py-2 px-4 rounded-md">Shop Now</button>
           </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="relative w-full">
+        <div className="relative w-full h-full">
           <img src={slideBg} alt="" className="w-full h-full object-cover"/>
-          <div className="absolute top-[20%] left-[10%] right-0 flex flex-col justify-center gap-12  items-center md:items-start">
-            <p className="text-5xl text-white leading-[60px]">We are fast and <br/><span className="text-[#FF931E]">reliable</span></p>
-            <button className="text-white bg-[#FF931E] text-[18px] py-2 px-4 rounded-md">Shop Now</button>
+          <div className="absolute top-[30%] left-[15%] right-0 flex flex-col justify-center gap-6 md:gap-12 items-start">
+            <p className="text-2xl md:text-5xl text-white leading-[30px] md:leading-[60px]">We are fast and <br/><span className="text-[#FF931E]">reliable</span></p>
+            <button className="text-white bg-[#FF931E] text-sm md:text-[18px] py-2 px-4 rounded-md">Shop Now</button>
           </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="relative w-full">
+        <div className="relative w-full h-full">
           <img src={slideBg} alt="" className="w-full h-full object-cover"/>
-          <div className="absolute top-[20%] left-[10%] right-0 flex flex-col justify-center gap-12  items-center md:items-start">
-            <p className="text-5xl text-white leading-[60px]">Your happiness is our <br/><span className="text-[#FF931E]">priority</span></p>
-            <button className="text-white bg-[#FF931E] text-[18px] py-2 px-4 rounded-md">Shop Now</button>
+          <div className="absolute top-[30%] left-[15%] right-0 flex flex-col justify-center gap-6 md:gap-12 items-start">
+            <p className="text-2xl md:text-5xl text-white leading-[30px] md:leading-[60px]">Your happiness is our <br/><span className="text-[#FF931E]">priority</span></p>
+            <button className="text-white bg-[#FF931E] text-sm md:text-[18px] py-2 px-4 rounded-md">Shop Now</button>
           </div>
         </div>
       </SwiperSlide>
