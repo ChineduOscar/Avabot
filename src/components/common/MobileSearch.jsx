99206150f3d45d87d4a25a1e20cdabadc5ea8d80
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2024-04-16 04:02:46 -0700

    fixed the search icon

--- src/components/common/MobileSearch.jsx
+++ src/components/common/MobileSearch.jsx
@@ -4,7 +4,7 @@ const MobileSearch = () => {
   return (
     <div className="flex items-center justify-center w-full py-1 px-2 bg-white">
     <input className="text-[#67563F] text-sm bg-[#FFEFDE] rounded-l-md p-2 w-full outline-0" type="text" placeholder="Search products, brands & Categories..." />
-    <button className="bg-[#FFEFDE] p-2 rounded-r-md"><SearchIcon /></button>
+    <button className="bg-[#FFEFDE] p-2 rounded-r-md"><SearchIcon color='black' /></button>
   </div>
   )
 }
